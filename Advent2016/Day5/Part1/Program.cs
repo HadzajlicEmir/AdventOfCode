00273d4e44a40ebadfb665cce6c614127aaeebee
HadzajlicEmir <emir29902@gmail.com>
2024-03-03 01:36:04 +0100

    Finished Day 5

---
+++ Advent2016/Day5/Part1/Program.cs
@@ -0,0 +1,36 @@
+﻿string sSourceData = "ugkcyxxp";
+int counter = -1;
+string password = "";
+bool whileControl = false;
+
+string CreateMD5(string input)
+{
+    // Use input string to calculate MD5 hash
+    using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+    {
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
+
+    }
+}
+
+for (int i = 0; i < 8; ++i)
+{   
+    whileControl = false;
+    do
+    {
+        ++counter;
+        string hash = CreateMD5(sSourceData + counter);
+        if (hash.StartsWith("00000"))
+        {
+            password += hash[5];
+            whileControl = true;
+        }
+    }
+    while (!whileControl);
+
+}
+
+Console.WriteLine(password);
---
+++ Advent2016/Day5/Part2/Program.cs
@@ -0,0 +1,49 @@
+﻿string sSourceData = "ugkcyxxp";
+int counter = -1;
+char[] password = new char[8];
+bool whileControl = false;
+
+string CreateMD5(string input)
+{
+    // Use input string to calculate MD5 hash
+    using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+    {
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
+    }
+}
+Array.Fill(password, '_');
+
+for (int i = 0; i < 8; ++i)
+{
+    whileControl = false;
+    do
+    {
+        ++counter;
+        string hash = CreateMD5(sSourceData + counter);
+        if (hash.StartsWith("00000"))
+        {
+            int value = -1;
+            if (Char.IsDigit(hash[5]))
+            {
+                value = (int)char.GetNumericValue(hash[5]);
+            }
+            if (value >= 0 && value < 8)
+            {
+                if (password[value] == '_')
+                {
+                    password[value] = hash[6];
+                    whileControl = true;
+                    foreach (var character in password)
+                    {
+                        Console.Write(character);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+    while (!whileControl);
+}
